17055e7fd12626db39e724d2e45b41dc52672567
Rafal Mika <rafal.mika@kaseya.com>
2023-01-18 10:01:14 +0100

    task1 done

---
+++ task1/api1/api.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/api1")
+def hello_world_from_api():
+    return "<p> Hello World from API 1 !!!</p>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=80)
