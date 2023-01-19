c1d603310764a94e4536f6a86eaed94e5385c741
Rafal Mika <rafal.mika@kaseya.com>
2023-01-19 11:43:18 +0100

    task2 done

--- task2/api/api.py
+++ task2/api/api.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 
-conn_string = os.getenv("CONNECTION_STRING")
+connection_string = os.getenv("POSTGRES_CONNECTION_STRING")
 
 
 def connect_to_db(conn_str):
@@ -13,14 +13,26 @@ def connect_to_db(conn_str):
     return conn
 
 
-@app.route("/is-db-connected")
-def is_db_connected():
-    with connect_to_db(conn_string) as connection:
-        return "<p>Connection is up</p>" if not connection.closed else "<p>Connection is down</p>"
-
 @app.route("/")
 def hello_world():
-    return "<p> Hellow World ! </p>"
+    return "<p> Hello World !</p>"
+
+
+@app.route("/db-version")
+def get_db_version():
+    with connect_to_db(connection_string) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute('SELECT version()')
+            version = cursor.fetchall()
+            return {"database":version[0][0]}
+
+
+@app.route("/is-db-connected")
+def get_db_connection_status():
+    with connect_to_db(connection_string) as connection:
+        status = "Connection is up" if not connection.closed else "Connection is down"
+        return {"connection status":status}
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
